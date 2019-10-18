19a713ff3a5b70b22d5d774e427b641b3a651a00
hoanguyen1203 <hoa.nguyen@production.com.vn>
2019-10-18 18:43:23 +0700

    feat: create speed test

--- script.js
+++ script.js
@@ -1,71 +1,74 @@
-const testWrapper = document.querySelector(".test-wrapper");
-const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
-const resetButton = document.querySelector("#reset");
-const theTimer = document.querySelector(".timer");
+const testWrapper = document.querySelector(".test-wrapper")
+const testArea = document.querySelector("#test-area")
+const originText = document.querySelector("#origin-text p").innerHTML
+const resetButton = document.querySelector("#reset")
+const theTimer = document.querySelector(".timer")
 
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function addLeadingZero(number) {
-    var string = number.toString();
-    var length = string.length;
+    var string = number.toString()
+    var length = string.length
     if ( length == 1) {
-        string = "0" + string;
+        string = "0" + string
     }
-    return string;
+    return string
 }
 
 // Run a standard minute/second/hundredths timer:
-var hundredths = 0;
-var second = 0;
-var minute = 0;
+var hundredths = 0
+var second = 0
+var minute = 0
 
 function start() {
     if(hundredths === 99) {
-        second++;
-        hundredths = 0;
+        second++
+        hundredths = 0
         if(second === 59) {
-            minute++;
-            second = 0;
+            minute++
+            second = 0
         }
     }
-    hundredths++;
-    theTimer.innerHTML = addLeadingZero(minute) + ":" + addLeadingZero(second) + ":" + addLeadingZero(hundredths);
+    hundredths++
+    theTimer.innerHTML = addLeadingZero(minute) + ":" + addLeadingZero(second) + ":" + addLeadingZero(hundredths)
 }
 
-//var startTime = setInterval(start, 10);
+var startTime = null
 testArea.oninput = function() {
-    var startTime = setInterval(start, 10);
+    if(!startTime){
+        startTime= setInterval(start, 10)
+    }
     if (checkText(testArea.value)) {
-        testWrapper.style.border = "10px solid green";
-        clearInterval(startTime);
+        testWrapper.style.border = "10px solid green"
+        clearInterval(startTime)
     } else {
-        testWrapper.style.border = "10px solid red";
+        testWrapper.style.border = "10px solid red"
     }
 };
 
 // Match the text entered with the provided text on the page:
 function checkText(text) {
     if(text == originText) {
-        return true;
+        return true
     }
-    return false;
+    return false
 }
 
 // Start the timer:
 
 // Reset everything:
-resetButton.onclick = function() {reset()};
+resetButton.onclick = function() {reset()}
 
 function reset() {
-    hundredths = 0;
-    second = 0;
-    minute = 0;
-    theTimer.innerHTML = addLeadingZero(minute) + ":" + addLeadingZero(second) + ":" + addLeadingZero(hundredths);
-    clearInterval(startTime);
+    hundredths = 0
+    second = 0
+    minute = 0
+    theTimer.innerHTML = addLeadingZero(minute) + ":" + addLeadingZero(second) + ":" + addLeadingZero(hundredths)
+    clearInterval(startTime)
+    startTime = null
 
-    testArea.value = "";
-    testWrapper.style.border = "10px solid grey";
+    testArea.value = ""
+    testWrapper.style.border = "10px solid grey"
 }
 
 // Event listeners for keyboard input and the reset button:
